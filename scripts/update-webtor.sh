c6ef0098dd4844e1d8b88ebd009d2fbc1e59d058
Bishan Vithanage <tvbishan@users.noreply.github.com>
2023-04-14 14:21:33 +0530

    url updated

--- scripts/update-webtor.sh
+++ scripts/update-webtor.sh
@@ -1,4 +1,4 @@
 sudo snap refresh microk8s --channel=1.22/stable
-curl -L  'https://raw.githubusercontent.com/webtor-io/helm-charts/master/helmfile.yaml' > helmfile.yaml
+curl -L  'https://raw.githubusercontent.com/tvbishan/helm-charts/master/helmfile.yaml' > helmfile.yaml
 helm repo up
 helmfile apply
