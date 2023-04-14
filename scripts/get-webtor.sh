36744f34cd4434580bc630fd1702605ee8bd16dd
Bishan Vithanage <tvbishan@users.noreply.github.com>
2023-04-14 14:22:13 +0530

    url updated

--- scripts/get-webtor.sh
+++ scripts/get-webtor.sh
@@ -15,7 +15,7 @@ chmod 0600 ~/.kube/config
 curl -L 'https://github.com/roboll/helmfile/releases/download/v0.143.0/helmfile_linux_arm64' > helmfile
 chmod +x helmfile
 sudo mv ./helmfile /usr/local/bin/
-curl -L  'https://raw.githubusercontent.com/webtor-io/helm-charts/master/helmfile.yaml' > helmfile.yaml
+curl -L  'https://raw.githubusercontent.com/tvbishan/helm-charts/master/helmfile.yaml' > helmfile.yaml
 helmfile apply
 sudo microk8s kubectl get svc torrent-http-proxy -o yaml -n webtor | grep http -A 1 | grep nodePort | head -1| awk '{print "Listening HTTP at port "  $2}'
 sudo microk8s kubectl get svc torrent-http-proxy -o yaml -n webtor | grep grpc -A 1 | grep nodePort | awk '{print "Listening GRPC at port "  $2}'
